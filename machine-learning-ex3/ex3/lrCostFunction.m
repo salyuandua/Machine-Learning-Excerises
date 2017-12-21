f6a01d86fe046341999a7ca385000a25a49bdf79
Li Xuejian <l18765827739@gmail.com>
2017-12-20 20:55:47 -0500

    12.20

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,18 +35,14 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-
-
-
-
-
-
-
-
-
+h0 = sigmoid(X*theta);
+J = (1/m)*sum(-y.*log(h0) - (1-y).*log(1-h0));
+grad = (1/m)*(X'*(h0-y));
+penalize=sum(theta(2:end).^2);
+J=J+(lambda/(2*m))*penalize;
+grad(2:end) = grad(2:end) + (lambda/m)*theta(2:end);
 
 % =============================================================
 
-grad = grad(:);
 
 end
